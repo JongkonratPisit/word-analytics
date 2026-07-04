324a4d9382b41b0040bd1ab6b9af826ce28fd75c
Jongkonrat <jongkonrat.p@aware.co.th>
2026-07-04 14:55:58 +0700

    three component and style

---
+++ src/container.jsx
@@ -0,0 +1,11 @@
+import Stat from "./Stats";
+import Textarea from "./Textarea";
+
+export default function container() {
+  return (
+    <main className="container">
+      <Textarea />
+      <Stat />
+    </main>
+  )
+}
